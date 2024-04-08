322f00aa891fd18df7e3764cc305f0648d95cf00
Pablo Gila-Herranz <pablo.gila.herranz@gmail.com>
2024-04-08 12:36:01 +0200

    Automatic update from Obsidian Notes on 2024-04-08 12:36:01

---
+++ update.sh
@@ -0,0 +1,17 @@
+# This script copies my current Obsidian notes on CASTEP as the README.md file, and pushes to GitHub.
+
+#!/bin/bash
+
+date=$(date +"%Y-%m-%d %H:%M:%S")
+
+cp "/home/pablo/Documents/obsidian/Work ⚛️/Instruments/CASTEP.md" README.md
+
+(zenity --info --text="CASTEP notes updated from Obsidian. Pushing to GitHub..." --timeout=1 --no-wrap --title="castep4dummies update") &
+
+git status
+git add .
+git commit -m "Automatic update from Obsidian Notes on $date"
+git push
+
+(zenity --info --text="Push completed!  :D" --timeout=1 --no-wrap --title="castep4dummies update") &
+
