4d21358ff43a1173bbcc4c5938f1a22dfae425a5
Pablo Gila-Herranz <pablo.gila.herranz@gmail.com>
2024-04-08 12:42:30 +0200

    Automatic update from Obsidian Notes on 2024-04-08 12:42:30

--- update.sh
+++ update.sh
@@ -6,14 +6,14 @@ date=$(date +"%Y-%m-%d %H:%M:%S")
 
 cp "/home/pablo/Documents/obsidian/Work ⚛️/Instruments/CASTEP.md" README.md
 
-(zenity --info --text="CASTEP notes updated. Pushing to GitHub..." --timeout=2 --no-wrap --title="castep4dummies update") &
+(zenity --info --text="CASTEP notes updated. Pushing to GitHub..." --timeout=1.5 --no-wrap --title="castep4dummies update") &
 
 git status
 git add .
 git commit -m "Automatic update from Obsidian Notes on $date"
 
 if [ $? -ne 0 ]; then
-    (zenity --error --text="Git commit aborted! Did you really modified the notes?" --no-wrap --title="castep4dummies update") &
+    (zenity --error --text="Git commit aborted! Did you really modified the notes?"--timeout=1.5 --no-wrap --title="castep4dummies update") &
     exit 2
 fi
 
@@ -21,9 +21,9 @@ git push
 
 # Check if the push was successful
 if [ $? -ne 0 ]; then
-    (zenity --error --text="Git push failed..." --no-wrap --title="castep4dummies update") &
+    (zenity --error --text="Git push failed..." --timeout=1.5 --no-wrap --title="castep4dummies update") &
     exit 2
 fi
 
-(zenity --info --text="Done!  :D" --timeout=2 --no-wrap --title="castep4dummies update") &
+(zenity --info --text="Done!  :D" --timeout=1 --no-wrap --title="castep4dummies update") &
 
